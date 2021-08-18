257d8d594c82903c1f96a72351e85f65dc1d055d
JoyceLei <81801688+LeiShu-BNU@users.noreply.github.com>
2021-08-18 15:38:48 +0800

    Update pred_ensemble.py

--- pred_ensemble.py
+++ pred_ensemble.py
@@ -28,7 +28,6 @@ from transformers.utils import logging
 RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)
 torch.manual_seed(RANDOM_SEED)
-k = 5
 device_ids=[0,1]
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(torch.cuda.is_available())
@@ -113,7 +112,7 @@ test_data_loader = create_data_loader(test, tokenizer, MAX_LEN, BATCH_SIZE)
 #pred_data_loader = create_pred_data_loader(pred, tokenizer, MAX_LEN, BATCH_SIZE)
 
 
-
+#BERT finetune模块==========================================================================
 class TitleClassifier(nn.Module):
     def __init__(self, n_classes):
         super(TitleClassifier, self).__init__()
@@ -130,20 +129,130 @@ class TitleClassifier(nn.Module):
         return self.out(output)
 
 # load the fine-tuned model
+#BERT+LSTM模块===================================================================================
+class bert_lstm(nn.Module):
+    def __init__(self, hidden_dim,output_size,n_layers,bidirectional=True, drop_prob=0.5):
+        super(bert_lstm, self).__init__()
+ 
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.hidden_dim = hidden_dim
+        self.bidirectional = bidirectional
+        
+        #Bert ----------------重点，bert模型需要嵌入到自定义模型里面
+        self.bert=BertModel.from_pretrained(PRE_TRAINED_MODEL_NAME)
+        for param in self.bert.parameters():
+            param.requires_grad = True
+        
+        # LSTM layers
+        self.lstm = nn.LSTM(768, hidden_dim, n_layers, batch_first=True, bidirectional=bidirectional)
+        
+        # dropout layer
+        self.dropout = nn.Dropout(drop_prob)
+        
+        # linear and sigmoid layers
+        if bidirectional:
+            self.fc = nn.Linear(hidden_dim*2, output_size)
+        else:
+            self.fc = nn.Linear(hidden_dim, output_size)
+          
+        #self.sig = nn.Sigmoid()
+ 
+    def forward(self, x, hidden):
+        batch_size = x.size(0)
+        #生成bert字向量
+        x=self.bert(x)[0]     #bert 字向量
+        
+        # lstm_out
+        #x = x.float()
+        lstm_out, (hidden_last,cn_last) = self.lstm(x, hidden)
+        #print(lstm_out.shape)   #[32,100,768]
+        #print(hidden_last.shape)   #[4, 32, 384]
+        #print(cn_last.shape)    #[4, 32, 384]
+        
+        #修改 双向的需要单独处理
+        if self.bidirectional:
+            #正向最后一层，最后一个时刻
+            hidden_last_L=hidden_last[-2]
+            #print(hidden_last_L.shape)  #[32, 384]
+            #反向最后一层，最后一个时刻
+            hidden_last_R=hidden_last[-1]
+            #print(hidden_last_R.shape)   #[32, 384]
+            #进行拼接
+            hidden_last_out=torch.cat([hidden_last_L,hidden_last_R],dim=-1)
+            #print(hidden_last_out.shape,'hidden_last_out')   #[32, 768]
+        else:
+            hidden_last_out=hidden_last[-1]   #[32, 384]
+            
+            
+        # dropout and fully-connected layer
+        out = self.dropout(hidden_last_out)
+        #print(out.shape)    #[32,768]
+        out = self.fc(out) #全连接就是线性
+        
+        return out
+    
+    def init_hidden(self, batch_size):
+        weight = next(self.parameters()).data
+        
+        number = 1
+        if self.bidirectional:
+            number = 2
+        
+        if (USE_CUDA):
+            hidden = (weight.new(self.n_layers*number, batch_size, self.hidden_dim).zero_().float().cuda(),
+                      weight.new(self.n_layers*number, batch_size, self.hidden_dim).zero_().float().cuda()
+                     )
+        else:
+            hidden = (weight.new(self.n_layers*number, batch_size, self.hidden_dim).zero_().float(),
+                      weight.new(self.n_layers*number, batch_size, self.hidden_dim).zero_().float()
+                     )
+        
+        return hidden
 
-loaded_models = []
-model_names = locals()
-for i in range(k):
-    model_names['model_%s' % i] = TitleClassifier(len(class_names))
-    model_names['model_%s' % i] = torch.nn.DataParallel(model_names['model_%s' % i])   
-    model_names['model_%s' % i] = model_names['model_%s' %i].cuda(device = device_ids[0])
-    model_names['model_%s' % i].load_state_dict(torch.load('the_%s_fold_best_state.bin'%i))
-    loaded_models.append(model_names['model_%s' % i])
-#加载了model0,model1,...modelk-1
+PRE_TRAINED_MODEL_NAME = 'sikubert'
+tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
+sents = list(test['text'])
+y_test = test['labels'].to_list()
 
-def get_probs(model, data_loader):#输出值用于返回混淆矩阵
+bert_model = BertModel.from_pretrained(PRE_TRAINED_MODEL_NAME)
+output_size = 2
+hidden_dim = 384   #768/2
+n_layers = 2
+bidirectional = True  #这里为True，为双向LSTM
+lr=learning_rate=2e-5
+class_names=[0,1]
+
+
+bertlstm_model = bert_lstm(hidden_dim, output_size,n_layers, bidirectional)                        
+bertlstm_model = bertlstm_model.cuda() 
+bertlstm_model.load_state_dict(torch.load('best_state_bertlstm0812.bin'))
+
+
+small_batches =[]
+sent_size = len(sents)
+step = 64
+for i in range(0,sent_size,step):
+    small_batches.append(sents[i:i+step])
+    
+
+def get_lstm_probs(model, sents):
     model = model.eval()
+    prediction_probs = []
+    sent_id=tokenizer(sents,padding=True,truncation=True,max_length=MAX_LEN,return_tensors="pt")
+  
+    pred_h = model.init_hidden(len(sents))
+    inputs = sent_id["input_ids"].cuda() 
+    pred_h = tuple([each.data for each in pred_h])
+    prob_list = []
+    with torch.no_grad(): 
+        output = model(inputs,pred_h) 
+        probs = F.softmax(output, dim=1)
+    prediction_probs.extend([line for line in probs.cpu().numpy()])
+    return prediction_probs
 
+def get_bert_probs(model, data_loader):#输出值用于返回混淆矩阵
+    model = model.eval()
     texts = []
     prediction_probs = []
 
@@ -152,27 +261,45 @@ def get_probs(model, data_loader):#输出值用于返回混淆矩阵
             texts = d["texts"]
             input_ids = d["input_ids"].to(device)
             attention_mask = d["attention_mask"].to(device)
-
             outputs = model(
                 input_ids=input_ids,
                 attention_mask=attention_mask
             )
             _, preds = torch.max(outputs, dim=1)
             probs = F.softmax(outputs, dim=1)
-
             texts.extend(texts)
             prediction_probs.extend([line for line in probs.cpu().numpy()])
-
-    
     return texts, prediction_probs 
+
+
+loaded_models = []
+model_names = locals()
+num_bert_models = 5
+for i in range(num_bert_models):
+    model_names['model_%s' % i] = TitleClassifier(len(class_names))
+    model_names['model_%s' % i] = torch.nn.DataParallel(model_names['model_%s' % i])   
+    model_names['model_%s' % i] = model_names['model_%s' %i].cuda(device = device_ids[0])
+    model_names['model_%s' % i].load_state_dict(torch.load('5fold/the_%s_fold_best_state.bin'%i))
+    loaded_models.append(model_names['model_%s' % i])
+#加载了model0,model1,...modelk-1
+loaded_models.append(bertlstm_model)
 
 all_test_size = len(test)
 avg_prob = [np.zeros(2, dtype = float) for i in range(all_test_size)]
-
+k = len(loaded_models)
 for model in loaded_models:
-    texts,prediction_probs = get_probs(model, test_data_loader)
-    for i in range(all_test_size):
-        avg_prob[i] += prediction_probs[i]/k
+    if "lstm" not in model:
+        texts,prediction_probs = get_probs(model, test_data_loader)
+        for i in range(all_test_size):
+            avg_prob[i] += prediction_probs[i]/k
+    else:
+        prediction_probs = []
+        for sents in small_batches:
+            prediction_probs.extend(pred_lstm_sent_list(bertlstm_model, sents))
+        for i in range(all_test_size):
+            avg_prob[i] += prediction_probs[i]/k
+
+
       
 y_pred = [int(round(i[1])) for i in avg_prob]
 print(y_pred)
@@ -183,7 +310,7 @@ id_ids = test["ids"]
 df_save = pd.DataFrame()
 df_save['id'] = id_ids
 df_save['label'] = y_pred
-df_save.to_csv('result_fold5.csv',index=False)
+df_save.to_csv('result_ensemble.csv',index=False)
 # with open("res.txt","w",encoding="utf-8")as rf:
 #     i = 0
 #     while i < len(texts):
